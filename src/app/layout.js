82ab2412aac90c7cb0d55560faf8956f0fadeb1c
unknown <gazinafisrafi.gnr@gmail.com>
2025-03-27 17:32:47 +0600

    fix: favicon issue fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,14 +8,28 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Gazi Nafis Rafi",
   description: "A portfolio website for Gazi Nafis Md Abdullah who is a MERN stack developer",
-  icons:{
-    icon: '/me.svg'
-  }
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+      </head>
       <body className={inter.className}>
         <ClientLayout>{children}</ClientLayout>
         <Toaster/>
